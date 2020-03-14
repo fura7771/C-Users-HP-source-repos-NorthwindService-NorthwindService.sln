113cd4ffff654afa1b518a082ce41b1b6c2fe759
PIBOP0117010 Сатторов Фуркатжон Хасан Угли <PIBOP0117010@sibupk.su>
2020-03-14 15:37:53 +0700

    commit

--- NorthwindService/ProductContext.cs
+++ NorthwindService/ProductContext.cs
@@ -1,7 +1,7 @@
-﻿using NorthwindService.Controllers;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindService.Controllers;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
