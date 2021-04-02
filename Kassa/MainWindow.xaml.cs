0c63ff0629dfcede7e7e880e034b896d6c164ebf
PIBOP0117010 Сатторов Фуркатжон Хасан Угли <PIBOP0117010@sibupk.su>
2021-04-02 09:03:37 +0700

    Commid

---
+++ Kassa/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Kassa;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gril
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Category.ItemsSource = KassaEntities.GetContext().Categories.ToList();
+
+
+        }
+      
+        private void ItsNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProductDetals product = new ProductDetals();
+            product.ShowDialog();
+            
+
+        }
+        
+    }
+}
